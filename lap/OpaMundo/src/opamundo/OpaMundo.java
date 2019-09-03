a18db64ff4f2dd2c3fc99f0a0aafa19d79608c5c
viniciusalopes <suporte@viniciusalopes.com.br>
2019-09-03 12:45:41 -0300

    Opa, mundo!

---
+++ lap/OpaMundo/src/opamundo/OpaMundo.java
@@ -0,0 +1,45 @@
+/*
+ * A licença MIT
+ *
+ * Copyright 2019 Viniciusalopes Tecnologia <suporte@viniciusalopes.com.br>.
+ *
+ * É concedida permissão, gratuitamente, a qualquer pessoa que obtenha uma cópia
+ * deste software e dos arquivos de documentação associados (o "Software"), para
+ * negociar o Software sem restrições, incluindo, sem limitação, os direitos de uso,
+ * cópia, modificação e fusão, publicar, distribuir, sublicenciar e/ou vender cópias
+ * do Software, e permitir que as pessoas a quem o Software é fornecido o façam,
+ * sujeitas às seguintes condições:
+ *
+ * O aviso de copyright acima e este aviso de permissão devem ser incluídos em
+ * todas as cópias ou partes substanciais do Software.
+ *
+ * O SOFTWARE É FORNECIDO "NO ESTADO EM QUE SE ENCONTRA", SEM NENHUM TIPO DE GARANTIA,
+ * EXPRESSA OU IMPLÍCITA, INCLUINDO, MAS NÃO SE LIMITANDO ÀS GARANTIAS DE COMERCIALIZAÇÃO,
+ * ADEQUAÇÃO A UM FIM ESPECÍFICO E NÃO VIOLAÇÃO. EM NENHUMA CIRCUNSTÂNCIA, OS AUTORES
+ * OU PROPRIETÁRIOS DE DIREITOS DE AUTOR PODERÃO SER RESPONSABILIZADOS POR QUAISQUER
+ * REIVINDICAÇÕES, DANOS OU OUTRAS RESPONSABILIDADES, QUER EM AÇÃO DE CONTRATO,
+ * DELITO OU DE OUTRA FORMA, DECORRENTES DE, OU EM CONEXÃO COM O SOFTWARE OU O USO
+ * OU OUTRAS NEGOCIAÇÕES NO PROGRAMAS.
+ * ------------------------------------------------------------------------------------------
+ * Projeto   : OpaMundo
+ * Criado em : 03/09/2019
+ * Autor     : Viniciusalopes (Vovolinux) <suporte@vovolinux.com.br>
+ * Finalidade: Mandar um Opa ae.
+ * ------------------------------------------------------------------------------------------
+ */
+package opamundo;
+
+/**
+ *
+ * @author vovostudio
+ */
+public class OpaMundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Opa, mundo! Belesma?");
+    }
+    
+}
