2f01fa34b618e5e5b372a3ab57044a3a7e8bbde2
viniciusalopes <suporte@viniciusalopes.com.br>
2019-09-17 21:18:27 -0300

    Lista 1 es

---
+++ ap/Exemplo2/src/exemplo2/Principal.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exemplo2;
+
+// ler Nome, sexo, peso, calcular o peso ideal e a diferença do peso atual para o peso ideal
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Variáveis
+        String nome;
+        char sexo;
+        double peso, peso_ideal, diferenca_de_peso;
+        
+        // Entrada
+        // Colher todos os dados do usuário
+
+        // Processamento
+        // Calcular o peso ideal, a diferenca de peso e decidir se ele está acima ou abaixo de acordo com o peso atual e o peso ideal
+        
+        // Saída
+        /*
+        Nome,
+        idade,
+        peso,
+        peso_ideal,
+        acima/abaixo
+         */
+    }
+
+}
