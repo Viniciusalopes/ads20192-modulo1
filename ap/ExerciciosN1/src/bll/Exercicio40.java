a2e727732c37fccda3bcee55a9526bea6dacd44d
viniciusalopes <suporte@viniciusalopes.com.br>
2019-09-22 04:31:03 -0300

    Exercicio40 N1

---
+++ ap/ExerciciosN1/src/bll/Exercicio40.java
@@ -0,0 +1,51 @@
+/*
+ * ---------------------------------------------------------------------------------------
+ * Licença   : MIT - Copyright 2019 Viniciusalopes (Vovolinux) <suporte@vovolinux.com.br>
+ * Criado em : 22/09/2019
+ * Projeto   : ExerciciosN1
+ * Finalidade: N1
+ * ---------------------------------------------------------------------------------------
+ */
+package bll;
+
+import java.util.Scanner;
+
+/**
+ * 40. Crie um programa que leia a idade de uma pessoa e que mostre a sua classe
+ * eleitoral:
+ */
+// a. Não-Eleitor: Abaixo de 16 anos.
+// b. Eleitor obrigatório: entre 18 e 65 anos.
+// c. Eleitor Facultativo: entre 16 e 18 anos e maior de 65 anos.
+//
+public class Exercicio40 {
+
+    public static void main(String[] args) {
+        vai();
+    }
+
+    public static void vai() {
+        // Variáveis
+        Scanner sc = new Scanner(System.in);
+        int idade;
+        String classe_eleitoral = "NÃO-ELEITOR";
+
+        // Entrada
+        System.out.println();
+        System.out.print("Informe a sua idade: ");
+        idade = sc.nextInt();
+
+        // Processamento
+        if (idade >= 16) {
+            if (idade < 18 || idade >= 65) {
+                classe_eleitoral = "ELEITOR FACULTATIVO";
+            } else if (idade >= 18 && idade < 65) {
+                classe_eleitoral = "ELEITOR OBRIGATÓRIO";
+            }
+        }
+
+        // Saída
+        System.out.println();
+        System.out.printf("Sua classe eleitoral: %s\n", classe_eleitoral);
+    }
+}
